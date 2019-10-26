5a2bf22e52f7fa577490e46d4397753dae20d9cd
mhamzic <mhamzic@gmail.com>
2019-10-26 09:25:25 +0200

    after Luke's review and refactoring

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 let friends = ["John", "Mary", "Brad", "Ann", "Steve"];
 
-for (let i = 0; i < friends.length; i++) {
-  console.log(friends[i].toLocaleUpperCase() + ":");
-  for (let j = 100; j >= 1; j--) {
-    if (j > 1) {
-      console.log(
-        `${j} lines of code in the file, ${j} lines of code; ${
-          friends[i]
-        } strikes one out, clears it all out, ${j -
-          1} lines of code in the file`
-      );
-    } else {
-      console.log(
-        `${j} line of code in the file, ${j} line of code; ${
-          friends[i]
-        } strikes one out, clears it all out, ${j - 1} line of code in the file`
-      );
-    }
+const lines = n => (n === 1 ? "line" : "lines");
+
+for (let friend of friends) {
+  console.log(friend.toLocaleUpperCase() + ":");
+  for (let j = 10; j >= 1; j--) {
+    console.log(
+      `${j} ${lines(j)} of code in the file, ${friend} strikes one out; ${j - 1 ||
+        "no more"} ${lines(j - 1)} of code in the file!`
+    );
   }
 }
